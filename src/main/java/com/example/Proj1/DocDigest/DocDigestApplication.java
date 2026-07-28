30022118183abfb8a40362a6ef301d62d2e63d87
ekuSML <ekusmlnmusic@gmail.com>
2026-07-28 15:09:35 +0530

    Initial commit

---
+++ src/main/java/com/example/Proj1/DocDigest/DocDigestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Proj1.DocDigest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocDigestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocDigestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Proj1/DocDigest/DocDigestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proj1.DocDigest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocDigestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
